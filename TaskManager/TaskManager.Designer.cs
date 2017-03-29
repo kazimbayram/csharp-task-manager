9976560e5839d6797ce498f9820bdba7c548264f
Kazım Bayram <kazim1001@gmail.com>
2017-03-29 17:31:16 +0300

    Add Kill Button

---
+++ TaskManager/TaskManager.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TaskManager
+{
+    partial class TaskManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(435, 136);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 177;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Width = 125;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.Location = new System.Drawing.Point(12, 168);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(435, 141);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(291, 315);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(372, 315);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(75, 23);
+            this.btnKill.TabIndex = 3;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // TaskManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 380);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "TaskManager";
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.TaskManager_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnKill;
+    }
+}
+
---
+++ TaskManager/TaskManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public partial class TaskManager : Form
+    {
+        public TaskManager()
+        {
+            InitializeComponent();
+
+        }
+
+        private void TaskManager_Load(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+
+        private void refresh()
+        {
+            listView1.Clear();
+            listView2.Clear();
+            foreach (Process p in Process.GetProcesses("."))
+            {
+                if (p.MainWindowTitle.Length > 0)//görev çubuğunda gösterilen uygulamalardır.
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.Text = p.ProcessName.ToString();
+                    item.SubItems.Add(p.MainWindowTitle.ToString());
+                    listView1.Items.Add(item);
+                }
+                else //Arka planda Çalısan uygulamaları getirir.
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.Text = p.ProcessName.ToString();
+                    item.SubItems.Add(p.SessionId.ToString());
+                    item.SubItems.Add(p.Id.ToString());
+                    listView2.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            kill(listView1.SelectedItems[0].Text);
+            refresh();
+        }
+
+        private void kill(string process)
+        {
+            try
+            {
+                Process[] proc = Process.GetProcessesByName(process);
+                proc[0].Kill();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
